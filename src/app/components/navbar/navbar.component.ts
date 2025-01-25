0162f0a75921f2fe9fea84e51400b4c2090c90cf
mohammadalibashiri5 <mohammadalibashiri5@yahoo.com>
2025-01-25 18:47:15 +0100

    adding the name of the user on navbar once they are logged in

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HomeComponent} from '../home/home.component';
 import {FooterComponent} from '../footer/footer.component';
+import {NgForOf, NgIf} from '@angular/common';
+import {RegisterService} from '../../services/register.service';
+import {LoginService} from '../../services/login.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  username: string = 'Guest'; // Default username
+
+  constructor(private userService: LoginService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe((data) => {
+      this.username = data.name;
+    });
+  }
+  isNotLoggedIn(): boolean {
+    return !sessionStorage.getItem('token');
+  }
+
+  links: { name: string; url: string }[] = [
+    { name: 'Home', url: '/' },
+    { name: 'Addictions', url: '/my-addictions' },
+    { name: 'Experiences', url: '/experiences' },
+    { name: 'Checkin', url: '/checkin' },
+  ];
+
 
 }
