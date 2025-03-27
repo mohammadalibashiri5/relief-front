11a9c65eb4456360314abb298ce107ac7a541ef4
mohammad.ali.bashiri <mohammad.ali.bashiri@accenture.com>
2025-03-27 17:06:18 +0100

    checkin Service in process and userdetails component added.

---
+++ src/app/components/user-detail/user-detail.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-user-detail',
+  imports: [
+
+  ],
+  templateUrl: './user-detail.component.html',
+  styleUrl: './user-detail.component.css'
+})
+export class UserDetailComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+
+
+
+
+}
