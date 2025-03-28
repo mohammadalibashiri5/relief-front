57aa1c6e6d8c117e8aed73b632190b9937c22178
mohammad.ali.bashiri <mohammad.ali.bashiri@accenture.com>
2025-03-28 15:22:47 +0100

    adding modal components

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+
+  @Output() close = new EventEmitter<void>();
+
+  closeModal(): void {
+    this.close.emit();
+  }
+}
